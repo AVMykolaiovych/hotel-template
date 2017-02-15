9e914ca6d1af7291ce3d4ae1a933549797424c61
avnicolaevich <avnicolaevich@gmail.com>
2017-02-16 01:16:12 +0200

    prev button

--- app/js/common.js
+++ app/js/common.js
@@ -15,22 +15,27 @@ $(function() {
 	$('.owl-carousel').owlCarousel({
 		items:1,
 		nav:true,
+		navText: [
+      "<i class='icon-left-open slider-icon'></i>",
+      "<i class='icon-right-open slider-icon'></i>"
+      ],
 		dots:true,
 		mouseDrag: false,
 		loop:true, //Зацикливаем слайдер
 		smartSpeed:1000, //Время движения слайда
-		// responsive:{ //Адаптация в зависимости от разрешения экрана
-		// 	0:{
-		// 		items:1
-		// 	},
-		// 	600:{
-		// 		items:1
-		// 	},
-		// 	1000:{
-		// 		items:1
-		// 	}
-		// }
+		responsive:{ //Адаптация в зависимости от разрешения экрана
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
 	});
+
 
 });
 
