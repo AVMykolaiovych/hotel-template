a1750d03832fa17988e6cef21d1dd1c8c3a72b33
avnicolaevich <avnicolaevich@gmail.com>
2017-02-28 21:49:09 +0200

    responsive first commit

--- app/js/common.js
+++ app/js/common.js
@@ -22,7 +22,8 @@ $(function() {
 		dots:true,
 		mouseDrag: false,
 		loop:true, //Зацикливаем слайдер
-		smartSpeed:1000, //Время движения слайда
+		smartSpeed:500, //Время движения слайда
+		responsiveClass:true,
 		responsive:{ //Адаптация в зависимости от разрешения экрана
 			0:{
 				items:1
